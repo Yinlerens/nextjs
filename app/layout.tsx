323e501f59c74ac1590cbb46c7da21d4fc245fc4
rdpnr_tang.pengcheng <rdpnr_tang.pengcheng@unisinsight.com>
2022-11-22 18:01:43 +0800

    feat:2.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
+import { AnalyticsWrapper } from './analytics';
 import './globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="zh">
       <head></head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <AnalyticsWrapper />
+      </body>
     </html>
   );
 }
